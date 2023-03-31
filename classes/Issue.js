f789ffdf8e7c6e3f05419dd5d7687ff8d4207a00
Alexander White <whitie@whitie.ru>
2023-03-31 21:11:57 +0300

    1.0.2

--- classes/Issue.js
+++ classes/Issue.js
@@ -1,12 +1,47 @@
 class Issue {
   constructor() {
-    // this.buttons = this.get_buttons()
-    // if (!this.buttons.length) return
+    this.edit_buttons = this.get_edit_buttons()
+    this.add_edit_buttons_listeners()
 
-    // this.add_listeners()
     this.issue_form = this.get_issue_form()
     if (!this.issue_form) return
     this.modify_issue_form()
+  }
+ 
+  get_edit_buttons() {
+    let edit_buttons = document.querySelectorAll('a[href^="/journals/"].icon-edit')
+    return edit_buttons
+  }
+
+  add_edit_buttons_listeners() {
+    if (!this.edit_buttons || !this.edit_buttons.length) return
+
+    this.edit_buttons.forEach(edit_button => {
+      edit_button.addEventListener('click', this.handle_button_click.bind(this, edit_button))
+    })
+  }
+
+  handle_button_click(edit_button) {
+    if (edit_button.extra_buttons_implemented) return
+    edit_button.extra_buttons_implemented = true
+    let parent = edit_button.parentNode.parentNode.parentNode
+    let timeout = setTimeout(this.check_form_presense.bind(this, parent, edit_button), 300)
+  }
+
+  check_form_presense(parent, edit_button) {
+    let close_buttons = parent.querySelectorAll('form[id^=journal-][id$=-form] p input, form[id^=journal-][id$=-form] p a')
+    this.modify_close_buttons(close_buttons, edit_button)
+    let buttons_row = parent.querySelector('.jstElements')
+    let editor_field = parent.querySelector('.jstEditor textarea')
+    this.add_extra_buttons(buttons_row, editor_field)
+  }
+
+  modify_close_buttons(close_buttons, edit_button) {
+    close_buttons.forEach(close_button => {
+      close_button.addEventListener('click', event => {
+        edit_button.extra_buttons_implemented = false
+      })
+    })
   }
 
   get_issue_form() {
@@ -78,7 +113,7 @@ class Issue {
     let link_text_label = document.createElement('label')
     link_text_label.setAttribute('for', 'link_text')
     link_text_label.classList.add('rdm-link-editor__link-text-label')
-    link_text_label.innerText = 'Link text:'
+    link_text_label.innerText = 'Text:'
 
     let link_text_input = document.createElement('input')
     link_text_input.type = 'text'
@@ -89,7 +124,7 @@ class Issue {
     let link_href_label = document.createElement('label')
     link_href_label.setAttribute('for', 'link_href')
     link_href_label.classList.add('rdm-link-editor__link-href-label')
-    link_href_label.innerText = 'Link address:'
+    link_href_label.innerText = 'Address:'
 
     let link_href_input = document.createElement('input')
     link_href_input.type = 'text'
@@ -133,56 +168,45 @@ class Issue {
     this.handle_close_editor(link_editor, button)
   }
 
-  create_modal(type, editor_field) {
-    let modal = document.createElement('div')
-    modal.classList.add('rdm-modal')
-    modal.classList.add(`rdm-modal_${type}`)
-    modal.id = `rdm-${type}`
+  // create_modal(type, editor_field) {
+  //   let modal = document.createElement('div')
+  //   modal.classList.add('rdm-modal')
+  //   modal.classList.add(`rdm-modal_${type}`)
+  //   modal.id = `rdm-${type}`
 
-    let modal_content = type == 'link' ? this.create_modal_content_link() : this.create_modal_content_table()
+  //   let modal_content = type == 'link' ? this.create_modal_content_link() : this.create_modal_content_table()
 
-    let close_button = document.createElement('div')
-    close_button.classList.add('rdm-modal__close')
-    close_button.innerText = '+'
-    close_button.addEventListener('click', this.handle_close_editor.bind(this, modal))
+  //   let close_button = document.createElement('div')
+  //   close_button.classList.add('rdm-modal__close')
+  //   close_button.innerText = '+'
+  //   close_button.addEventListener('click', this.handle_close_editor.bind(this, modal))
 
-    let buttons_row = document.createElement('div')
-    buttons_row.classList.add('rdm-link-editor__buttons')
+  //   let buttons_row = document.createElement('div')
+  //   buttons_row.classList.add('rdm-link-editor__buttons')
 
-    let paste_button = document.createElement('button')
-    paste_button.classList.add('rdm-link-editor__button')
-    paste_button.classList.add('rdm-link-editor__button_paste')
-    paste_button.innerText = 'Paste'
-    paste_button.addEventListener('click', this.handle_paste_button.bind(this, modal, editor_field))
+  //   let paste_button = document.createElement('button')
+  //   paste_button.classList.add('rdm-link-editor__button')
+  //   paste_button.classList.add('rdm-link-editor__button_paste')
+  //   paste_button.innerText = 'Paste'
+  //   paste_button.addEventListener('click', this.handle_paste_button.bind(this, modal, editor_field))
 
-    let cancel_button = document.createElement('button')
-    cancel_button.classList.add('rdm-link-editor__button')
-    cancel_button.classList.add('rdm-link-editor__button_cancel')
-    cancel_button.innerText = 'Cancel'
-    cancel_button.addEventListener('click', this.handle_close_editor.bind(this, modal))
+  //   let cancel_button = document.createElement('button')
+  //   cancel_button.classList.add('rdm-link-editor__button')
+  //   cancel_button.classList.add('rdm-link-editor__button_cancel')
+  //   cancel_button.innerText = 'Cancel'
+  //   cancel_button.addEventListener('click', this.handle_close_editor.bind(this, modal))
 
-    buttons_row.appendChild(cancel_button)
-    buttons_row.appendChild(paste_button)
-    modal_content.appendChild(buttons_row)
-    modal.appendChild(modal_content)
-    modal.appendChild(close_button)
-    document.querySelector('body').appendChild(modal)
-  }
-
-  create_modal_content_table() {}
-
-  // get_buttons() {
-  //   let buttons = document.querySelectorAll('a.icon-helpdesk-reply, a.icon-edit')
-  //   return buttons
+  //   buttons_row.appendChild(cancel_button)
+  //   buttons_row.appendChild(paste_button)
+  //   modal_content.appendChild(buttons_row)
+  //   modal.appendChild(modal_content)
+  //   modal.appendChild(close_button)
+  //   document.querySelector('body').appendChild(modal)
   // }
 
-  // add_listeners() {
-  //   this.buttons.forEach(button => {
-  //     button.addEventListener('click', this.handle_button_click)
-  //   })
-  // }
+  // create_modal_content_table() {}
 
-  // handle_button_click() {
-  //   alert('test')
-  // }
+  
+
+
 }
