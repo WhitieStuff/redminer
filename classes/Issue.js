1205c7bc0a1f61763087b0bbd94c38c4972813d8
Alexander White <whitie@whitie.ru>
2023-04-03 17:54:23 +0300

    1.0.3

--- classes/Issue.js
+++ classes/Issue.js
@@ -1,33 +1,41 @@
 class Issue {
   constructor() {
+    /** Buttons on each editable ticket message. */
     this.edit_buttons = this.get_edit_buttons()
     this.add_edit_buttons_listeners()
 
+    /** Main edit form for new message in ticket. */
     this.issue_form = this.get_issue_form()
     if (!this.issue_form) return
     this.modify_issue_form()
   }
- 
+
+  /** Returns edit buttons of each editable ticket message. */
   get_edit_buttons() {
     let edit_buttons = document.querySelectorAll('a[href^="/journals/"].icon-edit')
     return edit_buttons
   }
 
+  /** Adds listeners on each edit button. */
   add_edit_buttons_listeners() {
     if (!this.edit_buttons || !this.edit_buttons.length) return
 
     this.edit_buttons.forEach(edit_button => {
-      edit_button.addEventListener('click', this.handle_button_click.bind(this, edit_button))
+      edit_button.addEventListener('click', this.handle_edit_button_click.bind(this, edit_button))
     })
   }
 
-  handle_button_click(edit_button) {
+  /** Handles edit button click. */
+  handle_edit_button_click(edit_button) {
+    // If button has already been pressed.
     if (edit_button.extra_buttons_implemented) return
+    // Mark the button as pressed so that it wouldn't work on multi clicks.
     edit_button.extra_buttons_implemented = true
     let parent = edit_button.parentNode.parentNode.parentNode
     let timeout = setTimeout(this.check_form_presense.bind(this, parent, edit_button), 300)
   }
 
+  /** Checks whether the edit form is appended and adds the listeners. */
   check_form_presense(parent, edit_button) {
     let close_buttons = parent.querySelectorAll('form[id^=journal-][id$=-form] p input, form[id^=journal-][id$=-form] p a')
     this.modify_close_buttons(close_buttons, edit_button)
@@ -36,19 +44,35 @@ class Issue {
     this.add_extra_buttons(buttons_row, editor_field)
   }
 
+  /** Makes CLOSE and SAVE buttons mark the edit button as non-pressed. */
   modify_close_buttons(close_buttons, edit_button) {
     close_buttons.forEach(close_button => {
-      close_button.addEventListener('click', event => {
-        edit_button.extra_buttons_implemented = false
-      })
+      if (close_button.tagName == 'INPUT') {
+        let journal_id = `change-${close_button.parentNode.parentNode.id.split('-')[1]}`
+        close_button.addEventListener('click', event => {
+          setTimeout(this.handle_save_button.bind(this, journal_id), 1000)
+        })
+      } else {
+        close_button.addEventListener('click', event => {
+          edit_button.extra_buttons_implemented = false
+        })
+      }
     })
   }
 
+  /** Handles the save button click and readds the listener to edit button. */
+  handle_save_button(journal_id) {
+    let edit_button = document.querySelector(`#${journal_id} a[href^="/journals/"].icon-edit`)
+    edit_button.addEventListener('click', this.handle_edit_button_click.bind(this, edit_button))
+  }
+
+  /** Returns the main ticket form. */
   get_issue_form() {
     let form = document.querySelector('#issue-form')
     return form
   }
 
+  /** Modifies the main ticket form. */
   modify_issue_form() {
     let edit_blocks = this.issue_form.querySelectorAll('.jstBlock')
     edit_blocks.forEach(edit_block => {
@@ -58,6 +82,7 @@ class Issue {
     })
   }
 
+  /** Adds extra buttons to a form. */
   add_extra_buttons(buttons_row, editor_field) {
     let space = this.create_space()
     let spoiler_button = this.create_button('spoiler', buttons_row, editor_field, this.handle_spoiler_button)
@@ -68,6 +93,7 @@ class Issue {
     buttons_row.appendChild(link_button)
   }
 
+  /** Creates button with the given params. */
   create_button(type, buttons_row, editor_field, handler) {
     /** <button type="button" tabindex="200" class="jstb_h1" title="Heading 1"><span>Heading 1</span></button> */
     let button = document.createElement('button')
@@ -82,17 +108,20 @@ class Issue {
     return button
   }
 
+  /** Handles the spoiler button click. */
   handle_spoiler_button(editor_field) {
     let value = '\n{{collapse(Expand)\n\n}}'
     editor_field.value += value
   }
 
+  /** Handles the link button click. */
   handle_link_button(editor_field, buttons_row, button) {
     if (button.link_editor_implemented) return
     let link_modal = this.create_link_editor(editor_field, buttons_row, button)
     button.link_editor_implemented = true
   }
 
+  /** Returns the space element to implement between buttons. */
   create_space() {
     /** <span id="space4" class="jstSpacer">&nbsp;</span> */
     let space = document.createElement('span')
@@ -103,6 +132,7 @@ class Issue {
     return space
   }
 
+  /** Returns the link editor form. */
   create_link_editor(editor_field, buttons_row, button) {
     let parent = buttons_row.parentNode
     let grand_parent = parent.parentNode
@@ -154,11 +184,13 @@ class Issue {
     grand_parent.appendChild(link_editor, parent)
   }
 
+  /** Handes the link editor close button. */
   handle_close_editor(link_editor, button) {
     button.link_editor_implemented = false
     link_editor.remove()
   }
 
+  /** Hanles the link editor save button. */
   handle_paste_button(link_editor, editor_field, button) {
     let link_text = link_editor.querySelector('#link_text').value
     let link_href = link_editor.querySelector('#link_href').value
@@ -205,8 +237,4 @@ class Issue {
   // }
 
   // create_modal_content_table() {}
-
-  
-
-
 }
